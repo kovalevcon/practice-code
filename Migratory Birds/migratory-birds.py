a679c39e038d4a902be1cb82eb33869f6a4697a7
kovalevcon <kovalevcon@mail.ru>
2018-08-20 14:45:26 +0600

    Update solution `Migratory Birds` for Python language

--- Migratory Birds/migratory-birds.py
+++ Migratory Birds/migratory-birds.py
@@ -1,13 +1,19 @@
 # Complete the migratoryBirds function below.
 def migratoryBirds(arr):
-    arr_counts = [
-        arr.count(1),
-        arr.count(2),
-        arr.count(3),
-        arr.count(4),
-        arr.count(5)
-    ]
-    return 1 + arr_counts.index(max(arr_counts))
+    count = [0] * 6
+    for t in map(int, arr):
+        count[t] += 1
+
+    return count.index(max(count))
+
+    # arr_counts = [
+    #     arr.count(1),
+    #     arr.count(2),
+    #     arr.count(3),
+    #     arr.count(4),
+    #     arr.count(5)
+    # ]
+    # return 1 + arr_counts.index(max(arr_counts))
 
 
 print(migratoryBirds([1, 4, 4, 4, 5, 3]))  # 4
